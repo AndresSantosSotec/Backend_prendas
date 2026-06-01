7504433b8142885824fd73b4ae76c72635004933
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2026-06-01 14:19:51 -0600

    feat: Agregar dominio Avanza a CORS - avanzadigiprenda.stockgenius-sotecpro.com

--- config/cors.php
+++ config/cors.php
@@ -28,6 +28,7 @@ return [
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
         'https://digiprenda.inniserver.net',
+        'https://avanzadigiprenda.stockgenius-sotecpro.com',
         '*',
     ],
 
